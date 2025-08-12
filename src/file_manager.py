84fff926d6923798e2f0aa83b0d2b420e9012e90
Sina Moradi <sinamoradi002@gmail.com>
2025-08-12 12:42:48 +0330

    filemanager 1

---
+++ src/file_manager.py
@@ -0,0 +1,17 @@
+import json
+
+class FileManager:
+    def __init__(self):
+        pass
+    def load_file(self, file_path):
+        try:
+            json_data = json.load(open(file_path, 'r'))
+            return json_data
+        except FileNotFoundError as e:
+            print(e)
+            return None
+    def save_file(self, file_path , json_data):
+        try:
+            open(file_path, 'w').write(json.dumps(json_data))
+        except FileNotFoundError as e:
+            print(e)
